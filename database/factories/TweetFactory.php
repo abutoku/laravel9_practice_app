cd0246935dedbd6451cad0024f0c8b5c00505bf4
abutoku <ystkz2021@gmail.com>
2022-06-04 23:28:43 +0900

    update

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -17,7 +17,8 @@ class TweetFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => 1, //つぶやきを投稿したユーザーのIDをデフォルトで1とする
             'content' => $this->faker->realText(100)
-        ];
+            ];
     }
 }
